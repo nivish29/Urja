0b94b33f5c206b4f1a547fa3cceb29d435074434
Nihal <nihal.vish29@gmail.com>
2024-10-25 19:03:42 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['urja-self.vercel.app', '*'],
+        origin: ['https://urja-nufjtauij-nivish29s-projects.vercel.app', '*'],
         methods: ['GET', 'POST','PUT'],
     },
 });
@@ -28,7 +28,7 @@ const io = new Server(server, {
 // Middleware setup
 app.use(express.json());
 app.use(cors({
-    origin: 'urja-self.vercel.app',
+    origin: 'https://urja-nufjtauij-nivish29s-projects.vercel.app',
     methods: ['GET', 'POST','PUT'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
